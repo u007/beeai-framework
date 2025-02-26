2ea38b289fd0517fb112da8ac117ca06c7434e5f
Tomáš Dvořák <toomas2d@gmail.com>
2025-02-26 09:03:35 +0100

    feat: improve pre-commit hooks (#404)
    Signed-off-by: Tomas Dvorak <toomas2d@gmail.com>
    Co-authored-by: Lukáš Janeček <xjacka@users.noreply.github.com>

--- .githooks/hook.sh
+++ .githooks/hook.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 set -e
 
@@ -27,19 +27,23 @@ fi
 # Run hooks based on staged files
 if [ "$HAS_TS_FILES" -eq 1  ] && grep -q "\"git:$HOOK_NAME\"" "$TS_DIR/package.json"; then
   echo "Running $HOOK_NAME hook in $TS_DIR..."
-  (cd "$TS_DIR" && npm run "git:$HOOK_NAME" "$HOOK_ARGS") || exit $?
+  HOOK_ARGS_SCOPED=$(echo "$HOOK_ARGS" | sed 's/[^ ]* */..\/&/g' | sed 's/..\/typescript\///g')
+  (cd "$TS_DIR" && npm run "git:$HOOK_NAME" "$HOOK_ARGS_SCOPED") || exit $?
+  echo "$STAGED_EXISTING_FILES" | xargs -r git add
 fi
 
 if [ "$HAS_PY_FILES" -eq 1 ]; then
   echo "Running Python $HOOK_NAME hook..."
-  (cd "$PY_DIR" && poetry run poe git --hook "$HOOK_NAME" "$HOOK_ARGS") || exit $?
+  HOOK_ARGS_SCOPED=$(echo "$HOOK_ARGS" | sed 's/[^ ]* */..\/&/g' | sed 's/..\/python\///g')
+  (cd "$PY_DIR" && poetry run poe git --hook "$HOOK_NAME" "$HOOK_ARGS_SCOPED") || exit $?
+  echo "$STAGED_EXISTING_FILES" | xargs -r git add
 fi
 
 # General commit style check
 if [ "$HAS_TS_FILES" -eq 0 ] && [ "$HAS_PY_FILES" -eq 0 ]; then
   if [ "$HOOK_NAME" = "commit-msg" ] ; then
     COMMIT_MSG=$(cat "$COMMIT_MSG_FILE")
-    CONVENTIONAL_COMMIT_REGEX="^(chore|ci|docs|revert)(\(\w+\))?!?: .+"
+    CONVENTIONAL_COMMIT_REGEX="^(feat|fix|chore|ci|docs|revert)(\(\w+\))?!?: .+"
 
     if ! echo "$COMMIT_MSG" | grep -qE "$CONVENTIONAL_COMMIT_REGEX"; then
       echo "Error: The provided commit message does not adhere to conventional commit style!"
