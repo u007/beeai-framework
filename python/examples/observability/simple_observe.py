1b215ee7d1c4c4357d084f5a95a42849b105acab
Angelo Danducci <angelo.danducci.ii@ibm.com>
2025-03-13 10:07:06 -0400

    docs(examples): adds on and observe examples for llm create (#561)
    * docs(examples): adds on and observe examples to llm create
    
    Signed-off-by: AngeloDanducci <angelo.danducci.ii@ibm.com>
    
    * refactor(examples): use new event name for observability examples
    
    Signed-off-by: AngeloDanducci <angelo.danducci.ii@ibm.com>
    
    ---------
    
    Signed-off-by: AngeloDanducci <angelo.danducci.ii@ibm.com>

---
+++ python/examples/observability/simple_observe.py
@@ -0,0 +1,63 @@
+import asyncio
+import sys
+import traceback
+
+from beeai_framework import SystemMessage, UserMessage
+from beeai_framework.backend.chat import ChatModel
+from beeai_framework.errors import FrameworkError
+from beeai_framework.memory.unconstrained_memory import UnconstrainedMemory
+from examples.helpers.io import ConsoleReader
+
+reader = ConsoleReader()
+
+
+async def main() -> None:
+    memory = UnconstrainedMemory()
+    await memory.add_many(
+        [
+            SystemMessage("Always respond very concisely."),
+            UserMessage("Give me the first 5 prime numbers."),
+        ]
+    )
+
+    llm = ChatModel.from_name("ollama:granite3.1-dense:8b")
+
+    response = (
+        await llm.create(messages=memory.messages, stream=True)
+        .observe(
+            lambda emitter: emitter.on(
+                "start",
+                lambda data, event: print(
+                    "On start", *(message.to_plain() for message in data["input"].messages), sep="\n"
+                ),
+            )
+        )
+        .observe(
+            lambda emitter: emitter.on(
+                "new_token",
+                lambda data, event: print(
+                    "On new_token", *(message.to_plain() for message in data["value"].messages), sep="\n"
+                ),
+            )
+        )
+        .observe(
+            lambda emitter: emitter.on(
+                "success",
+                lambda data, event: print(
+                    "On success", *(message.to_plain() for message in data["value"].messages), sep="\n"
+                ),
+            )
+        )
+        .observe(lambda emitter: emitter.on("error", lambda data, event: print(event.name, data["error"])))
+        .observe(lambda emitter: emitter.on("finish", lambda data, event: print("On finish", data)))
+    )
+
+    print("Response:", response.get_text_content())
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except FrameworkError as e:
+        traceback.print_exc()
+        sys.exit(e.explain())
---
+++ python/examples/observability/simple_on.py
@@ -0,0 +1,57 @@
+import asyncio
+import sys
+import traceback
+
+from beeai_framework import SystemMessage, UserMessage
+from beeai_framework.backend.chat import ChatModel
+from beeai_framework.errors import FrameworkError
+from beeai_framework.memory.unconstrained_memory import UnconstrainedMemory
+from examples.helpers.io import ConsoleReader
+
+reader = ConsoleReader()
+
+
+async def main() -> None:
+    memory = UnconstrainedMemory()
+    await memory.add_many(
+        [
+            SystemMessage("Always respond very concisely."),
+            UserMessage("Give me the first 5 prime numbers."),
+        ]
+    )
+
+    llm = ChatModel.from_name("ollama:granite3.1-dense:8b")
+
+    response = (
+        await llm.create(messages=memory.messages, stream=True)
+        .on(
+            "start",
+            lambda data, event: print(
+                event.name, *(message.to_plain() for message in data["input"].messages), sep="\n"
+            ),
+        )
+        .on(
+            "new_token",
+            lambda data, event: print(
+                event.name, *(message.to_plain() for message in data["value"].messages), sep="\n"
+            ),
+        )
+        .on(
+            "success",
+            lambda data, event: print(
+                event.name, *(message.to_plain() for message in data["value"].messages), sep="\n"
+            ),
+        )
+        .on("error", lambda data, event: print(event.name, data["error"]))
+        .on("finish", lambda data, event: print(event.name, data))
+    )
+
+    print("Response:", response.get_text_content())
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except FrameworkError as e:
+        traceback.print_exc()
+        sys.exit(e.explain())
