fe3ec04290e8e4e0f21592448f0f6c9a6da38845
Milan Gallas <44726162+GALLLASMILAN@users.noreply.github.com>
2025-03-18 12:18:15 +0100

    feat(instrumentation): export modules for instrumentation (#605)
    Signed-off-by: Milan Gallas <gallas.milan@gmail.com>

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -20,3 +20,6 @@ export * from "./errors.js";
 export * from "./version.js";
 export * from "./serializer/serializer.js";
 export * from "./agents/base.js";
+export * from "./backend/chat.js";
+export * from "./backend/embedding.js";
+export * from "./tools/base.js";
